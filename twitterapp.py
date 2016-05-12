69adb240cbbfabdd4d979b67f8bd86d66e20a1fb
bhawana <vermabhawana50@gmail.com>
2016-05-13 00:31:51 +0530

    -- bug fixes

--- twitterapp.py
+++ twitterapp.py
@@ -30,7 +30,7 @@ mysql = MySQL()
 
 #MySQL Configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'cutiepie07'
+app.config['MYSQL_DATABASE_PASSWORD'] = '****'
 app.config['MYSQL_DATABASE_DB'] = 'twitter_clone'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
